a5572d64688d263e3296317567610882c050429e
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2026-03-09 20:41:34 -0300

    Atividade

---
+++ variaveisOperadores.js
@@ -0,0 +1,15 @@
+let num_A = 10;
+let num_B = 5;
+let resultado;
+
+resultado = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${resultado}`);
+
+resultado = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${resultado}`);
+
+resultado = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${resultado}`);
+
+resultado = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${resultado}`);
